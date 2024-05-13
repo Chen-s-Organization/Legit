81e727b2faa3ba2a88982c66e9e5b17fb31cee3a
chenberger <chen10.berger@gmail.com>
2024-05-13 18:54:07 +0300

    added some comments

--- server/webhooksHandler.py
+++ server/webhooksHandler.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 class GitHubWebhookHandler:
+        
     def handle_push_code_event(self, payload):
         try:
             head_commit = payload.get('head_commit')
@@ -8,7 +9,7 @@ class GitHubWebhookHandler:
                 timestamp_str = head_commit.get('timestamp')
                 if timestamp_str:
                     timestamp = datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%S%z')
-                    if 14 <= timestamp.hour < 16:
+                    if 14 <= timestamp.hour < 16: # test if the push event occurred between 14:00-16:00
                         self.notify_about_suspicious_behavior(suspicious_behavior_message="Push event occurred between 14:00-16:00")
         except Exception as e:
             print("Error processing push event:", str(e))
@@ -28,7 +29,7 @@ class GitHubWebhookHandler:
             repository_name = payload.get('repository').get('name')
             deleted_at = datetime.strptime(datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'), '%Y-%m-%dT%H:%M:%SZ')
             created_at = datetime.strptime(payload.get('repository').get('created_at'), '%Y-%m-%dT%H:%M:%SZ')
-            if (deleted_at - created_at).total_seconds() < 600:
+            if (deleted_at - created_at).total_seconds() < 600: # test if the repository was created and deleted within 10 minutes
                 self.notify_about_suspicious_behavior(suspicious_behavior_message="Creating and deleting a repository within 10 minutes, the repository name is: " + repository_name)
         except Exception as e:
             print("Error processing repository event:", str(e))
