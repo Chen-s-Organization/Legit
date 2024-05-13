45716a2d85f19c52776ce37390b70cb7fb993613
chenberger <chen10.berger@gmail.com>
2024-05-13 18:49:39 +0300

    remove print

--- server/webhooksHandler.py
+++ server/webhooksHandler.py
@@ -8,7 +8,6 @@ class GitHubWebhookHandler:
                 timestamp_str = head_commit.get('timestamp')
                 if timestamp_str:
                     timestamp = datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%S%z')
-                    print("Push event occurred at:", timestamp.hour)
                     if 14 <= timestamp.hour < 20:
                         self.notify_about_suspicious_behavior(suspicious_behavior_message="Push event occurred between 14:00-16:00")
         except Exception as e:
